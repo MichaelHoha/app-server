dce00a79b8c0a27c08be0bb47c69ebe27be52c7f
michaelHoha <michael1hoha@gmail.com>
2024-04-23 21:25:33 +0300

    Minor fix

--- config/database.js
+++ config/database.js
@@ -1,8 +1,19 @@
-const { Sequelize } = require('sequelize')
-const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
-export const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'postgres',
-  logging: false,
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
