330166d156f9fda0125f7d6d6a5636691edc04cf
michaelHoha <michael1hoha@gmail.com>
2024-04-24 00:24:40 +0300

    minor

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { createServer } = require("node:http");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
 const codeBlockRoutes = require("./routes/codeBlockRoutes");
 const codeBlockController = require("./controllers/codeBlockController");
 
 const app = express();
-const server = createServer(app);
+const server = createServer(app); // Create HTTP server using 'http' module
 
 // Middleware
 app.use(cors());
@@ -15,11 +15,13 @@ app.use(express.json());
 // Routes
 app.use("/api", codeBlockRoutes);
 
-// Start server
+// Start server by attaching Express app to HTTP server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
+  // Use 'server.listen' instead of 'app.listen'
   console.log(`Server is running on port ${PORT}`);
 });
 
+// Initialize Socket.IO server
 const io = new Server(server);
 codeBlockController.setupSocketIO(io);
