871ef8c46f3f3a09b248bf1f66e36be1157f99c0
michaelHoha <michael1hoha@gmail.com>
2024-04-23 21:49:42 +0300

    Minor

--- models/codeblock.js
+++ models/codeblock.js
@@ -1,19 +1,10 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class CodeBlock extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  CodeBlock.init({
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+class CodeBlock extends Model {}
+
+CodeBlock.init(
+  {
     title: DataTypes.STRING,
     content: DataTypes.TEXT,
     participentsCount: DataTypes.INTEGER
@@ -21,6 +12,8 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'CodeBlock',
     tableName: 'code_blocks'
-  });
-  return CodeBlock;
-};
+  }
+);
+
+// the defined model is the class itself
+console.log(CodeBlock === sequelize.models.CodeBlock);
