ac31e0b1544032c79ccc98c3f5e0fa6b4d5818fc
michaelHoha <michael1hoha@gmail.com>
2024-04-24 16:14:58 +0300

    minor

--- controllers/codeBlockController.js
+++ controllers/codeBlockController.js
@@ -70,7 +70,7 @@ function setupSocketIO(io) {
           if (codeBlock) {
             codeBlock.participants_count = 0;
             codeBlock.update({ participants_count });
-            io.emit("setPraticipantesCount", { id, participants_count });
+            io.emit("praticipantesCountDown", { id, participants_count });
             console.log(
               "Client participants_countparticipants_countparticipants_count " +
                 participants_count
